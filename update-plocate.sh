db80346f0412329379cfde7a6aa0de8fd731ce4a
Steinar H. Gunderson <steinar+git@gunderson.no>
2020-10-12 00:22:38 +0200

    Make update-plocate.sh work properly if installed to /usr.

--- update-plocate.sh
+++ update-plocate.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-/usr/local/sbin/plocate-build /var/lib/mlocate/mlocate.db /var/lib/mlocate/plocate.db.new
+SBINDIR=@sbindir@
+[ -d $SBINDIR ] || SBINDIR=/usr/local/sbin  # Default if not processed by Meson.
+
+$SBINDIR/plocate-build /var/lib/mlocate/mlocate.db /var/lib/mlocate/plocate.db.new
 chgrp mlocate /var/lib/mlocate/plocate.db.new
 mv /var/lib/mlocate/plocate.db.new /var/lib/mlocate/plocate.db
