d0c4fc8ee820e487064c7f8b4ba9f695e19052ea
Steinar H. Gunderson <steinar+git@gunderson.no>
2020-10-14 23:31:08 +0200

    Add some options for controlling installation and processing of the cron.daily script.

--- update-plocate.sh
+++ update-plocate.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-SBINDIR=@sbindir@
-[ -d $SBINDIR ] || SBINDIR=/usr/local/sbin  # Default if not processed by Meson.
+if [ @PROCESSED_BY_MESON@ = 1 ]; then
+	SBINDIR=@sbindir@
+	LOCATEGROUP=@locategroup@
+else
+	SBINDIR=/usr/local/sbin
+	LOCATEGROUP=mlocate
+fi
 
 $SBINDIR/plocate-build /var/lib/mlocate/mlocate.db /var/lib/mlocate/plocate.db.new
-chgrp mlocate /var/lib/mlocate/plocate.db.new
+chgrp $LOCATEGROUP /var/lib/mlocate/plocate.db.new
 mv /var/lib/mlocate/plocate.db.new /var/lib/mlocate/plocate.db
