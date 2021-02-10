da00fbf8e69391ce561cfec6da543b9726137870
Steinar H. Gunderson <steinar+git@gunderson.no>
2021-02-10 21:39:37 +0100

    Install CACHEDIR.tag in /var/lib/plocate.
    This helps certain tools understand that everything in the directory
    is autogenerated. Suggested by Marco d'Itri.

--- mkdir.sh
+++ mkdir.sh
@@ -1,2 +1,9 @@
 #! /bin/sh
 mkdir -p ${DESTDIR}/"$1"
+
+cat <<EOF >${DESTDIR}/"$1"/CACHEDIR.TAG
+Signature: 8a477f597d28d172789f06886806bc55
+# This file is a cache directory tag created by plocate.
+# For information about cache directory tags, see:
+#	https://bford.info/cachedir/
+EOF
